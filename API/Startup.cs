021522ca47f48b88053fdd75c0c68808499322a3
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-11-25 11:10:47 +0300

    section 3 end - client skeleton added

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
 
             services.AddDbContext<DataContext>(options =>
             {
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            // need to be in here
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
